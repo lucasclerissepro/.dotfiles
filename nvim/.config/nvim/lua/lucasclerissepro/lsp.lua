9a536fa3da7eb67f8fa3e39f7a167f4dee963f13
Lucas Clerisse <lucasclerisse@protonmail.com>
2022-07-17 21:19:03 +0800

    add telescope for lsp symbols

--- nvim/.config/nvim/lua/lucasclerissepro/lsp.lua
+++ nvim/.config/nvim/lua/lucasclerissepro/lsp.lua
@@ -2,99 +2,99 @@ local cmp = require('cmp')
 local luasnip = require('luasnip')
 
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 -- Compe configuration used for autocompletion
 cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      luasnip.lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            luasnip.lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
 
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
 
 
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
 
-  sources = cmp.config.sources({
-    { name = 'luasnip' }, -- For luasnip users.
-    { name = 'nvim_lsp' },
-    -- { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-  }, {
-    { name = 'buffer' },
-  })
+    sources = cmp.config.sources({
+        { name = 'luasnip' }, -- For luasnip users.
+        { name = 'nvim_lsp' },
+        -- { name = 'vsnip' }, -- For vsnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }, {
+        { name = 'buffer' },
+    })
 })
 
 local function config(_config)
-  return vim.tbl_deep_extend("force", {
-    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-    on_attach = function(client)
-      Nnoremap("gd", ":lua vim.lsp.buf.definition()<CR>")
-      Nnoremap("K", ":lua vim.lsp.buf.hover()<CR>")
-      Nnoremap("<leader>vws", ":lua vim.lsp.buf.workspace_symbol()<CR>")
-      Nnoremap("<leader>vd", ":lua vim.diagnostic.open_float()<CR>")
-      Nnoremap("[d", ":lua vim.lsp.diagnostic.goto_next()<CR>")
-      Nnoremap("]d", ":lua vim.lsp.diagnostic.goto_prev()<CR>")
-      Nnoremap("<leader>vca", ":lua vim.lsp.buf.code_action()<CR>")
-      Nnoremap("<leader>vrr", ":lua vim.lsp.buf.references()<CR>")
-      Nnoremap("<leader>vrn", ":lua vim.lsp.buf.rename()<CR>")
-      Inoremap("<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+    return vim.tbl_deep_extend("force", {
+        capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+        on_attach = function(client)
+            Nnoremap("gd", ":lua vim.lsp.buf.definition()<CR>")
+            Nnoremap("K", ":lua vim.lsp.buf.hover()<CR>")
+            Nnoremap("<leader>vws", ":lua vim.lsp.buf.workspace_symbol()<CR>")
+            Nnoremap("<leader>vd", ":lua vim.diagnostic.open_float()<CR>")
+            Nnoremap("[d", ":lua vim.lsp.diagnostic.goto_next()<CR>")
+            Nnoremap("]d", ":lua vim.lsp.diagnostic.goto_prev()<CR>")
+            Nnoremap("<leader>vca", ":lua vim.lsp.buf.code_action()<CR>")
+            Nnoremap("<leader>vrr", ":lua vim.lsp.buf.references()<CR>")
+            Nnoremap("<leader>vrn", ":lua vim.lsp.buf.rename()<CR>")
+            Inoremap("<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
 
-      require("lsp-format").on_attach(client)
-    end,
-  }, _config or {})
+            require("lsp-format").on_attach(client)
+        end,
+    }, _config or {})
 end
 
 -- Golang
 vim.fn.setenv("GOFLAGS", "-tags=unix")
 require("lspconfig").gopls.setup(config({
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-      },
-      staticcheck = true,
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+            },
+            staticcheck = true,
+        },
     },
-  },
 }))
 
 -- go install github.com/nametake/golangci-lint-langserver@latest
--- nvim/.config/nvim/plugin/telescope.vim
+++ nvim/.config/nvim/plugin/telescope.vim
@@ -3,3 +3,6 @@ nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
 nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
 nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
 nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+
+" For LSP server
+nnoremap <leader>fs <cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>
